87970fd7a45756fcc68683204c71c57172686ee0
the-lundy <78132416+the-lundy@users.noreply.github.com>
2021-01-28 18:35:52 -0700

    Initial Upload
    First Upload

---
+++ board.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << ' a b c d e f g h ' << std::endl;
+    std::cout <<   0 0 0 0 0 0 0 0 << std::endl;
+
+
+
+
+    // std::cout << "The board is " << board << std::endl;
+    return 0;
+}
