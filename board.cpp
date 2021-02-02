1dbe9d00c88608a96fa61cf04cbf8ccd4a1af472
the-lundy <78132416+the-lundy@users.noreply.github.com>
2021-02-01 20:55:34 -0700

    Added Class
    Put Class into board.cpp

--- board.cpp
+++ board.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+// #include <piece.h>
+class Piece {
+    private:
+        
+    public:
+        int posx, posy, value;
+    char pieceType; //Pawn Knight Bishop Rook Queen King
+    char color; //White Black
+		
+};
 
-int main()
-{
-    std::cout << ' a b c d e f g h ' << std::endl;
-    std::cout <<   0 0 0 0 0 0 0 0 << std::endl;
 
 
-
-
+int main(){
+    Piece pawn;
+    pawn.posx = 0;
+    pawn.posy = 0;
+    pawn.value = 1;
+    pawn.pieceType = 'P';
     // std::cout << "The board is " << board << std::endl;
+    std::cout << pawn.pieceType << std::endl;
     return 0;
 }
+
