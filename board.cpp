862ae6bff092fad3aa1b8937127a036817ee6e38
the-lundy <78132416+the-lundy@users.noreply.github.com>
2021-02-02 22:10:48 -0700

    Working With piece.h
    Got the class working and able to call and modify the class.h file

--- board.cpp
+++ board.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
-// #include <piece.h>
-class Piece {
-    private:
-        
-    public:
-        int posx, posy, value;
-    char pieceType; //Pawn Knight Bishop Rook Queen King
-    char color; //White Black
-		
-};
+#include "piece.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+void printI(int arg){
+    cout << arg << endl;
+}
+void printC(char arg){
+    cout << arg << endl;
+}
+void printBoard(int arg[8][8]){
+    //Less then 64 since we know the size of the chessboard
+    for (int ii = 0; ii < 8; ii++){
+        for ( int jj = 0; jj < 8; jj++){
+            
+
+            if (jj == 7){
+                cout << arg[ii][jj] <<endl;
+            } else{
+                cout << arg[ii][jj];
+            }
+        }
+
+    } 
+}
+
 
 
 
 int main(){
-    Piece pawn;
-    pawn.posx = 0;
-    pawn.posy = 0;
-    pawn.value = 1;
-    pawn.pieceType = 'P';
+    Pawn pawn1;
+    pawn1.posx = 0;
+    pawn1.posy = 0;
+    // pawn1.value = 10;
+    pawn1.color = 'W';
+    pawn1.boardPos[1][1] = 1;
     // std::cout << "The board is " << board << std::endl;
-    std::cout << pawn.pieceType << std::endl;
+    // for (size ii = 0; ii < sizeof(pawn1.boardPos); ii++){
+    // printC(pawn1.color);
+    printBoard(pawn1.boardPos);
+    // cout << pawn1 << endl;
+    // }
+
     return 0;
 }
 
